4d804f7367c6b57bb2918157d1db38263d6a9d0d
m6hdix <m6hdixx@gmail.com>
2023-05-15 10:18:53 +0430

    🔧 :add search Component

---
+++ src/Components/ModalSearch/index.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 25%;
+  height: 100vh;
+  position: absolute;
+  z-index: 1500;
+  padding: 10px;
+  background-color: #fff;
+  top: 0;
+`;
+
+export default function ModalSearch() {
+  return <Container>
+    
+  </Container>;
+}
