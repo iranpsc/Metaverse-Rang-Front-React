7db5f70f2855e8057f41e6c0bd4ea5d3c6eb4022
m6hdix <m6hdixx@gmail.com>
2023-05-15 10:18:59 +0430

    🔧 :add search Component

--- src/Pages/Search/index.jsx
+++ src/Pages/Search/index.jsx
@@ -1,1 +1,7 @@
-export default function Search(second) {third}
+import ModalSearch from "../../Components/ModalSearch";
+
+export default function Search() {return(
+    <ModalSearch>
+        
+    </ModalSearch>
+)}
