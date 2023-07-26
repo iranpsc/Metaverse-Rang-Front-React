91921dc8143f6318cbf03d1ef4b515608946f9da
m6hdix <m6hdixx@gmail.com>
2023-07-26 08:45:29 +0330

    ✅ :fix email Signup

--- src/Pages/Signup/index.jsx
+++ src/Pages/Signup/index.jsx
@@ -1,92 +1,143 @@
-import React, { useState } from 'react';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import Input from '../../Components/Inputs/Input';
-import Modal from '../../Components/Modal';
-import Form from '../../Components/Form/index.jsx';
-import useRequest from '../../Services/Hooks/useRequest';
-import Submit from '../../Components/Buttons/Submit.jsx';
+import React, { useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import Input from "../../Components/Inputs/Input";
+import Modal from "../../Components/Modal";
+import Form from "../../Components/Form/index.jsx";
+import useRequest from "../../Services/Hooks/useRequest";
+import Submit from "../../Components/Buttons/Submit.jsx";
+import styled from "styled-components";
+import GmailIcon from "../../Assets/images/gmail.png";
+
+const BodyEmail = styled.div`
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 3%;
+`;
+const BoxEmailNavigate = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 88%;
+  background-color: #e9e9e9;
+  border-radius: 5px;
+  gap: 5px;
+  margin-top: 1rem;
+`;
 
 export default function Signup() {
   const [searchParams] = useSearchParams();
 
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    referral: searchParams.get('referral'),
+    name: "",
+    email: "",
+    password: "",
+    referral: searchParams.get("referral"),
   });
 
-  const [message, setMessage] = useState('');
+  const [emailVerification, setEmailVerification] = useState(false);
 
   const { Request, HTTP_METHOD } = useRequest();
   const navigation = useNavigate();
 
   const onSubmitHandler = () => {
-    Request('register', HTTP_METHOD.POST, formData)
-      .then(() => {
-        navigation('/metaverse/successful/signup');
+    Request("register", HTTP_METHOD.POST, formData)
+      .then((res) => {
+        if (res.status == 201) {
+          setEmailVerification(true);
+        }
       })
       .catch((error) => {
         if (error?.response?.status === 422) {
           setMessage(error?.response?.data?.message);
         }
-    });
+      });
   };
 
   return (
-    <Modal title='ثبت نام'>
-      <Form onSubmit={onSubmitHandler}>
-        <Input
-          name='name'
-          type='text'
-          className='mt-5'
-          placeholder='نام کاربری میتوان نام شرکت و یا برند باشد'
-          value={formData.name}
-          dispatch={setFormData}
-        />
+    <Modal title="ثبت نام">
+      {!emailVerification ? (
+        <Form onSubmit={onSubmitHandler}>
+          <Input
+            name="name"
+            type="text"
+            className="mt-5"
+            placeholder="نام کاربری میتوان نام شرکت و یا برند باشد"
+            value={formData.name}
+            dispatch={setFormData}
+          />
 
-        <Input
-          name='email'
-          type='email'
-          placeholder='name@example.com'
-          value={formData.email}
-          dispatch={setFormData}
-        />
+          <Input
+            name="email"
+            type="email"
+            placeholder="name@example.com"
+            value={formData.email}
+            dispatch={setFormData}
+          />
 
-        <Input
-          name='password'
-          type='password'
-          placeholder='********'
-          value={formData.password}
-          dispatch={setFormData}
-        />
+          <Input
+            name="password"
+            type="password"
+            placeholder="********"
+            value={formData.password}
+            dispatch={setFormData}
+          />
+          <Submit text="ثبت نام" type="secondary" />
 
-        {message && (
-          <p className='w-100 text-right mb-4 text-danger'>{message}</p>
-        )}
+          <Link to="/metaverse/login" className="link text-2 mt-3 ">
+            ثبت نام کرده ام' وارد شوید'
+          </Link>
 
-        <Submit text='ثبت نام' type='secondary' />
+          <p className="text-information mt-4">
+            با کلیک بر روی دکمه ورود به سامانه موافقت میکنید
+          </p>
 
-        <Link to='/metaverse/login' className='link text-2 mt-3 '>
-          ثبت نام کرده ام' وارد شوید'
-        </Link>
-        
-        <p className='text-information mt-4'>
-          با کلیک بر روی دکمه ورود به سامانه موافقت میکنید
-        </p>
+          <a
+            href="https://rgb.irpsc.com/overview"
+            className="link text-1 mt-2"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            شرایط قرارداد خدمات
+          </a>
 
-        <a href='https://rgb.irpsc.com/overview' className='link text-1 mt-2' target={"_blank"} rel="noreferrer">
-          شرایط قرارداد خدمات
-        </a>
+          <p className="text-information mt-4">
+            سئوالی دارید یا میخواهید بیشتر بدانید؟
+          </p>
 
-        <p className='text-information mt-4'>
-          سئوالی دارید یا میخواهید بیشتر بدانید؟
-        </p>
+          <a href="https://rgb.irpsc.com/" className="link text-1 mt-2 pb-2">
+            .از وبسایت ما دیدن کنید
+          </a>
+        </Form>
+      ) : (
+        < >
+          <h2 className="mt-5">ایمیل خود را تایید کنید</h2>
+          <span className="mt-2">ما یک ایمیل به </span>
+          <span className="mt-2">{formData.email}</span>
+          <span className="mt-4">برای شروع روی لینک داخل ایمیل کلیک کنید </span>
+          <BoxEmailNavigate >
+            <img src={GmailIcon} alt="gmail" style={{ width: "40px" }} />
+            <a
+              href="https://mail.google.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              باز کردن جیمیل
+            </a>
+          </BoxEmailNavigate>
+          <span  className="mt-2"> ایمیل را دوباره ارسال کن </span>
+          <p className="text-information mt-5">
+            سئوالی دارید یا میخواهید بیشتر بدانید؟
+          </p>
 
-        <a href='https://rgb.irpsc.com/' className='link text-1 mt-2 pb-2'>
-          .از وبسایت ما دیدن کنید
-        </a>
-      </Form>
+          <a href="https://rgb.irpsc.com/" className="link text-1 mt-2 pb-5">
+            .از وبسایت ما دیدن کنید
+          </a>
+        </>
+      )}
     </Modal>
   );
 }
