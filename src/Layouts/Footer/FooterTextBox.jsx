0e8b2b3edde7001d1d27bced5db859ebfca033c4
m6hdix <m6hdixx@gmail.com>
2024-10-05 16:31:55 +0330

    remove old code

--- src/Layouts/Footer/FooterTextBox.jsx
+++
@@ -1,7 +0,0 @@
-export default function FooterTextBox({ className, Text }) {
-    return (
-        <div className={`footer-value white-box-shadow text-success text-center text-bold ${className}`}>
-            <p>{Text}</p>
-        </div>
-    )
-}
