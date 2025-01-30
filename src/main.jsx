4dc1c6d456dec14e8db8308def453fdb229e6ddd
m6hdix <m6hdixx@gmail.com>
2025-01-30 10:56:43 +0330

    remove Sentry initialization and related configurations, and clean up build script in package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import * as Sentry from "@sentry/react";
+// import * as Sentry from "@sentry/react";
 
-const isDebugEnabled =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "frdevelop.irpsc.com";
+// const isDebugEnabled =
+//   window.location.hostname === "localhost" ||
+//   window.location.hostname === "frdevelop.irpsc.com";
 
-Sentry.init({
-  dsn: "https://c71a2b88c754680dc5e405ccce46ffdf@sentry.hamravesh.com/7902",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Performance and debugging
-  enabled: "production",
-  debug: isDebugEnabled,
-  release: "1.0.0",
-  dist: "1",
-  attachStacktrace: true,
-  autoSessionTracking: true,
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [
-    "localhost",
-    "https://frdevelop.irpsc.com/metaverse/",
-  ],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+// Sentry.init({
+//   dsn: "https://c71a2b88c754680dc5e405ccce46ffdf@sentry.hamravesh.com/7902",
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.replayIntegration(),
+//   ],
+//   // Performance and debugging
+//   enabled: "production",
+//   debug: isDebugEnabled,
+//   release: "1.0.0",
+//   dist: "1",
+//   attachStacktrace: true,
+//   autoSessionTracking: true,
+//   // Tracing
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: [
+//     "localhost",
+//     "https://frdevelop.irpsc.com/metaverse/",
+//   ],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
