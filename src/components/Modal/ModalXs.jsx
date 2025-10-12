abb309781d962dc44927c7d00dcd34d20f1dda83
parsa <154906383+parsabr80@users.noreply.github.com>
2025-10-12 18:40:37 +0330

    fix some style

--- src/components/Modal/ModalXs.jsx
+++ src/components/Modal/ModalXs.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Header from "../Header/Header";
 import { useEffect } from "react";
 const Container = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
