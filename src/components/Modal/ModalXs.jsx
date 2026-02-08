9960f71340812cee5405b8dd64bf278671252b43
parsa <154906383+parsabr80@users.noreply.github.com>
2026-02-08 19:03:50 +0330

    fix modal position & fix layout

--- src/components/Modal/ModalXs.jsx
+++ src/components/Modal/ModalXs.jsx
@@ -2,15 +2,16 @@ import styled from "styled-components";
 import Header from "../Header/Header";
 
 const Container = styled.div`
- 
-  width: 100%;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
   background-color: rgba(0, 0, 0, 0.713);
-  z-index: 1000;
-   position: fixed;
-    height: 100%;
+  z-index: 9999;
 `;
 const Modal = styled.div`
   background-color: ${(props) => props.theme.colors.newColors.shades.bg2};
@@ -21,12 +22,9 @@ const Modal = styled.div`
   padding: 15px 20px;
 `;
 
-
-const ModalXs = ({ children, title, handleExitClick,onClose  }) => {
-
-
+const ModalXs = ({ children, title, handleExitClick, onClose }) => {
   return (
-    <Container onClick={onClose} > 
+    <Container onClick={onClose}>
       <Modal>
         <Header title={title} handleExit={handleExitClick} />
         {children}
