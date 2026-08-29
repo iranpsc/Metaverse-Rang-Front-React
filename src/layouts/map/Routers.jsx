660dc7a0f0f1c5c0ed534b442d9b20f0e4c55e86
parsa <154906383+parsabr80@users.noreply.github.com>
2026-08-29 09:41:19 +0330

    fix camelCase name

--- src/layouts/map/Routers.jsx
+++ src/layouts/map/Routers.jsx
@@ -64,7 +64,7 @@ import { UserContext } from "../../services/reducers/UserContext";
 
 export default function Routers() {
   const [user] = useContext(UserContext);
-
+console.log("metaverse is here")
   return (
     <Routes>
       <Route path="" element={<MainLayout />}>
